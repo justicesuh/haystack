d466d1a345756475fbd39198ef78b0886f860d4f
Justice Suh <justice.suh@gmail.com>
2025-09-30 14:53:36 -0400

    Configure tests and coverage

---
+++ haystack/tests/test_math.py
@@ -0,0 +1,16 @@
+def add(x: int, y: int) -> None:
+    return x + y
+
+
+def subtract(x: int, y: int) -> None:
+    return x - y
+
+
+def test_add() -> None:
+    assert add(2, 2) == 4
+    assert add(-2, 3) == 1
+
+
+def test_subtract() -> None:
+    assert subtract(10, 4) == 6
+    assert subtract(2, 7) == -5
