f162c3115483e215d1dbd679edd416674096da5f
Justice Suh <justice.suh@gmail.com>
2025-10-01 18:27:04 -0400

    Set up template routing

--- haystack/urls.py
+++ haystack/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
+from haystack.users import views as user_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', user_views.index),
 ]
