2fd8b873c0e3e40766f55b404b4e708cb2262858
Justice Suh <justice.suh@gmail.com>
2025-09-30 21:38:36 -0400

    Add UserManager

--- haystack/users/models.py
+++ haystack/users/models.py
@@ -1,8 +1,35 @@
-from django.contrib.auth.models import AbstractUser
+from typing import Any, ClassVar
+
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 from haystack.core.models import UUIDMixin
+
+
+class UserManager(BaseUserManager['User']):
+    def _create_user_object(self, email: str, password: str, **extra_fields: Any) -> 'User':
+        if email is None:
+            raise ValueError('Users must have an email address.')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.password = make_password(password)
+        return user
+
+    def _create_user(self, email: str, password: str, **extra_fields: Any) -> 'User':
+        user = self._create_user_object(email, password, **extra_fields)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email: str, password: str, **extra_fields: Any) -> 'User':
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email: str, password: str, **extra_fields: Any) -> 'User':
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser, UUIDMixin):
@@ -11,6 +38,8 @@ class User(AbstractUser, UUIDMixin):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    objects: ClassVar[UserManager] = UserManager()  # type: ignore[assignment]
 
     def __str__(self) -> str:
         return self.email
