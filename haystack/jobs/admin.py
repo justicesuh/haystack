87f5756c5e97f4c5656b387fb778cbe2f7d1f743
Justice Suh <justice.suh@gmail.com>
2025-10-01 19:57:03 -0400

    Create job models

---
+++ haystack/jobs/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from haystack.core.admin import UUIDModelAdmin
+from haystack.jobs.models import Company, Job, Location
+
+admin.site.register(Company, UUIDModelAdmin)
+admin.site.register(Job, UUIDModelAdmin)
+admin.site.register(Location, UUIDModelAdmin)
