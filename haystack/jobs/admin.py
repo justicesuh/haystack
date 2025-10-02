cd27e3077a77858be47bb503e2d11c43b970b39c
Justice Suh <justice.suh@gmail.com>
2025-10-02 16:29:50 -0400

    Display geo_id in admin

--- haystack/jobs/admin.py
+++ haystack/jobs/admin.py
@@ -3,6 +3,12 @@ from django.contrib import admin
 from haystack.core.admin import UUIDModelAdmin
 from haystack.jobs.models import Company, Job, Location
 
+
+class LocationAdmin(UUIDModelAdmin):
+    list_display = ('name', 'geo_id')
+    search_fields = ('name',)
+
+
 admin.site.register(Company, UUIDModelAdmin)
 admin.site.register(Job, UUIDModelAdmin)
-admin.site.register(Location, UUIDModelAdmin)
+admin.site.register(Location, LocationAdmin)
