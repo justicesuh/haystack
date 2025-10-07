7f9966eb2aff4ed276ac6dcf5d035ffb5b833c21
Justice Suh <justice.suh@gmail.com>
2025-10-07 15:40:10 -0400

    Create UUIDModel

--- haystack/core/fields.py
+++ haystack/core/fields.py
@@ -1,6 +1,9 @@
+import uuid
 from typing import Any
 
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils.functional import Promise
 
 
 class AutoCreatedField(models.DateTimeField):
@@ -16,4 +19,31 @@ class AutoUpdatedField(models.DateTimeField):
 
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         kwargs.setdefault('auto_now', True)
+        super().__init__(*args, **kwargs)
+
+
+class UUIDField(models.UUIDField):
+    """A UUIDField with default arguments."""
+
+    def __init__(
+        self,
+        verbose_name: str | Promise | None = None,
+        primary_key: bool = False,
+        version: int = 4,
+        editable: bool = False,
+        *args: Any,
+        **kwargs: Any,
+    ) -> None:
+        if version == 2:
+            raise ValidationError('UUID version 2 is not supported.')
+
+        if version < 1 or version > 5:
+            raise ValidationError('UUID version is not valid.')
+
+        default = getattr(uuid, f'uuid{version}')
+
+        kwargs.setdefault('verbose_name', verbose_name)
+        kwargs.setdefault('primary_key', primary_key)
+        kwargs.setdefault('default', default)
+        kwargs.setdefault('editable', editable)
         super().__init__(*args, **kwargs)
--- haystack/core/models.py
+++ haystack/core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-from haystack.core.fields import AutoCreatedField, AutoUpdatedField
+from haystack.core.fields import AutoCreatedField, AutoUpdatedField, UUIDField
 
 
 class TimestampedMixin(models.Model):
@@ -8,6 +9,22 @@ class TimestampedMixin(models.Model):
 
     created_at = AutoCreatedField()
     updated_at = AutoUpdatedField()
+
+    class Meta:
+        abstract = True
+
+
+class UUIDMixin(models.Model):
+    """Abstract base model that provides uuid field."""
+
+    uuid = UUIDField(_('UUID'))
+
+    class Meta:
+        abstract = True
+
+
+class UUIDModel(TimestampedMixin, UUIDMixin):
+    """Abstract base model that combines `TimeStampedMixin` and `UUIDMixin`."""
 
     class Meta:
         abstract = True
