9ab2bd8d131d056b70e45ad6e02ead9732160404
Justice Suh <justice.suh@gmail.com>
2025-10-08 16:05:37 -0400

    Register Job models in admin

--- haystack/jobs/admin.py
+++ haystack/jobs/admin.py
@@ -1,1 +1,8 @@
-# Register your models here.
+from django.contrib import admin
+
+from haystack.core.admin import UUIDModelAdmin
+from haystack.jobs.models import Company, Job, Location
+
+admin.site.register(Company, UUIDModelAdmin)
+admin.site.register(Location, UUIDModelAdmin)
+admin.site.register(Job, UUIDModelAdmin)
