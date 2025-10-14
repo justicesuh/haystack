7ddd81fc8e9ebbc2350425750951799030b9ba98
Justice Suh <justice.suh@gmail.com>
2025-10-14 14:10:50 -0400

    Add overload for get_parser factory

--- haystack/search/parsers/__init__.py
+++ haystack/search/parsers/__init__.py
@@ -1,7 +1,13 @@
-from typing import Any
+from typing import Any, Literal, overload
 
 from haystack.search.parsers.base import BaseParser, IPParser
 from haystack.search.parsers.linkedin import LinkedInParser
+
+
+@overload
+def get_parser(name: Literal['ip'], *args: Any, **kwargs: Any) -> IPParser: ...
+@overload
+def get_parser(name: Literal['linkedin'], *args: Any, **kwargs: Any) -> LinkedInParser: ...
 
 
 def get_parser(name: str, *args: Any, **kwargs: Any) -> BaseParser:
