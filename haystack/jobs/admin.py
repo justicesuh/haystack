04470f0834ea268a3fc1838c769321cc98e6e274
Justice Suh <justice.suh@gmail.com>
2025-10-15 19:33:47 -0400

    Customize Job admin

--- haystack/jobs/admin.py
+++ haystack/jobs/admin.py
@@ -3,6 +3,14 @@ from django.contrib import admin
 from haystack.core.admin import UUIDModelAdmin
 from haystack.jobs.models import Company, Job, Location
 
+
+class JobAdmin(UUIDModelAdmin):
+    list_display = ('title', 'easy_apply', 'populated')
+    list_filter = ('easy_apply', 'populated', 'status')
+
+    show_facets = admin.ShowFacets.ALWAYS
+
+
 admin.site.register(Company, UUIDModelAdmin)
 admin.site.register(Location, UUIDModelAdmin)
-admin.site.register(Job, UUIDModelAdmin)
+admin.site.register(Job, JobAdmin)
